8966a37925e96ff65b1cc40f5f56ba991f3e687f
Viraat <viraat.laldas@gmail.com>
2018-09-15 18:44:09 -0400

    commmit with venv

--- venv/Title.py
+++ venv/Title.py
@@ -2,28 +2,30 @@ from googlesearch import search
 import urllib2
 from BeautifulSoup import BeautifulSoup
 from nltk.tag import pos_tag
+import flask
 
 def parseNouns(title):
     tagged_sent = pos_tag(title.split())
     nouns = [word for word,pos in tagged_sent if pos == 'NNP']
     return nouns
 
-website_url = "https://www.ibc.org/consumption/trump-is-good-news-for-cnn/3248.article"
-soup = BeautifulSoup(urllib2.urlopen(website_url))
-title = soup.title.string
-title = title[:title.find('|')]
-print title
+def method ():
+    website_url = "https://www.ibc.org/consumption/trump-is-good-news-for-cnn/3248.article"
+    soup = BeautifulSoup(urllib2.urlopen(website_url))
+    title = soup.title.string
+    title = title[:title.find('|')]
+    print title
 
-query =  parseNouns(title)
-for i, w in enumerate(query):
-    query[i] =  w.replace('u',"")
+    query =  parseNouns(title)
+    for i, w in enumerate(query):
+        query[i] =  w.replace('u',"")
 
-query = "".join(query)
-print query
+    query = "".join(query)
+    print query
 
-print ("article title is " + title)
+    print ("article title is " + title)
 
-for url in search(query, stop=20):
-    print(url)
+    for url in search(query, stop=20):
+        print(url)
 
 
