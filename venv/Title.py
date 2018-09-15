300a79e07040c3483225cd7b75e51196907b62ed
Viraat <viraat.laldas@gmail.com>
2018-09-15 13:46:50 -0400

    Prints out URLs based off the title on Google

--- venv/Title.py
+++ venv/Title.py
@@ -1,6 +1,17 @@
+from googlesearch import search
 import urllib2
 from BeautifulSoup import BeautifulSoup
 
-website_url = "https://weather.com/storms/hurricane/news/2018-09-14-hurricane-florence-forecast-north-carolina-south-carolina-southeast"
+
+website_url = "http://www.foxnews.com/politics/2018/09/15/california-will-launch-its-own-damn-satellite-as-swipe-at-trump-governor-moonbeam-says.html"
 soup = BeautifulSoup(urllib2.urlopen(website_url))
-print soup.title.string
+
+# to search
+query = soup.title.string
+query = query[:query.find('|')]
+print ("article title is " + query)
+
+for url in search(query, stop=20):
+    print(url)
+
+
