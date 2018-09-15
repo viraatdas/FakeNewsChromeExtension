09e242e6a35fc19fc5136ce122b334bf92d362bb
Automage <pranav92000@gmail.com>
2018-09-15 13:58:50 -0400

    parseNouns funciton implemented
    Nouns can now be parsed using parseNouns()

--- venv/Title.py
+++ venv/Title.py
@@ -1,6 +1,17 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
+from nltk.tag import pos_tag
 
-website_url = "https://weather.com/storms/hurricane/news/2018-09-14-hurricane-florence-forecast-north-carolina-south-carolina-southeast"
+def parseNouns(title):
+    tagged_sent = pos_tag(title.split())
+    nouns = [word for word,pos in tagged_sent if pos == 'NNP']
+    return nouns
+
+website_url = "https://www.bloomberg.com/news/articles/2018-09-14/trump-said-to-want-200-billion-in-china-tariffs-despite-talks"
 soup = BeautifulSoup(urllib2.urlopen(website_url))
-print soup.title.string
+title = soup.title.string
+print title
+
+print parseNouns(title)
+
+
