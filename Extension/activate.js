6047bfad07f19ac2c7659f2403cdc2a1cab99c02
Tom Yarrow <tyarrow1@gmail.com>
2018-09-15 19:56:16 -0400

    First attempt at uploading URL json

--- Extension/activate.js
+++ Extension/activate.js
@@ -1,4 +1,5 @@
 var currentUrl;
+var urlJSON;
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
 function(tabs){
     	currentUrl = tabs[0].url;
@@ -9,4 +10,16 @@ function postURLfunction(){
 	//run the (url-using) function here
 	chrome.extension.getBackgroundPage().console.log(currentUrl);
 	document.getElementById("Title1").innerHTML = currentUrl;
+	urlJSON = JSON.stringify(currentUrl);
+	uploadURL;
 }
+function uploadURL() {
+    // ajax the JSON to the server
+    $.ajax({
+    type: 'POST',
+    url: 'localhost:5000',
+    data: urlJSON,
+    success: function(data) { alert('data: ' + data); },
+    contentType: "application/json",
+    dataType: 'json'
+});
