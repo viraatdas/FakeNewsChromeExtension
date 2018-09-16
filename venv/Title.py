92556a95ccef38743a746212ec9bdf1813add12f
Viraat Das <viraat.laldas@gmail.com>
2018-09-16 05:33:42 -0400

    combining changes and bodies of text works

--- venv/Title.py
+++ venv/Title.py
@@ -1,39 +1,78 @@
-from googlesearch import search
-import urllib2
-from BeautifulSoup import BeautifulSoup
+# -*- coding: utf-8 -*-
 from nltk.tag import pos_tag
-from routes import url
+from googlesearch import search
+from bs4 import BeautifulSoup
+import requests
 
-
-def parseNouns(title):
-    tagged_sent = pos_tag(title.split())
-    nouns = [word for word,pos in tagged_sent if pos == 'NNP']
-    return nouns
-
-def parseASCII(text):
-    chars = ['.', ',', '/', '-', '!', '?', '\'', '"']
-
-    for i, w in enumerate(text):
-        for j,l in enumerate(chars):
-            w = w.replace(l,'')
-        text[i] = w
-
-    return text
-
-def method ():
-    website_url = "http://www.foxnews.com/politics/2018/09/15/california-will-launch-its-own-damn-satellite-as-swipe-at-trump-governor-moonbeam-says.html"
-    soup = BeautifulSoup(urllib2.urlopen(website_url))
+def function (website_url):
+    soup = BeautifulSoup(requests.get(website_url).content, "html.parser")
     title = soup.title.string
     title = title[:title.find('|')]
-    print ("Article title: "+title)
 
-    query = parseNouns(title)
+    tagged_sent = pos_tag(title.split())
+    nouns = [word for word, pos in tagged_sent if pos == 'NNP']
+    query = nouns
+
+    def parseASCII(text):
+        chars = ['.', ',', '/', '-', '!', '?', '\'', '"']
+
+        for i, w in enumerate(text):
+            for j, l in enumerate(chars):
+                w = w.replace(l, '')
+            text[i] = w
+
+        return text
+
     query = parseASCII(query)
+    query = "".join(query)
 
-    query = " ".join(query)
-    print ("Query: "+query)
+    site_prefix = ["site:foxnews.com ", "site: businessinsider.com ", "site: wsj.com ", "sites: nytimes.com ", "site: washingtonpost.com "]
 
-    for url in search(query, stop=20):
-        print(url)
+    query_list = []
+    i = 0
+    while i<5:
+        query_list.append(site_prefix[i] + query)
+        i+=1
+    print (query_list)
 
-print method()
+    url_list_fox=[]
+    url_list_business = []
+    url_list_wall = []
+    url_list_new = []
+    url_list_washington = []
+
+    for url in search(query_list[0], stop=5):
+        (url_list_fox.append(url))
+
+    for url in search(query_list[1], stop=5):
+        url_list_business.append(url)
+
+    for url in search(query_list[2], stop=5):
+        url_list_wall.append(url)
+
+    for url in search(query_list[3], stop=5):
+        url_list_new.append(url)
+
+    for url in search(query_list[4], stop=5):
+        url_list_washington.append(url)
+
+
+    url_list_fox = url_list_fox[0]
+    url_list_business = url_list_business[0]
+    url_list_wall = url_list_wall[0]
+    url_list_new = url_list_new[0]
+    url_list_washington = url_list_washington[0]
+
+    def par (website_url):
+        page = requests.get(website_url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        page = soup.findAll('p')
+        return page
+
+    body_of_text_fox = par(url_list_fox)
+    body_of_text_business = par(url_list_business)
+    body_of_text_wall = par(url_list_wall)
+    body_of_text_new = par(url_list_new)
+    body_of_text_washington = par(url_list_washington)
+
+
